9ba829dafcb1ea01601cc46da029824b02ea1605
unknown <anisalidia493@gmail.com>
2026-06-13 20:53:32 +0700

    set homepage redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  	return (
-    	<main className="min-h-screen bg-slate-50 flex flex-col items-center justify-center p-4">
-      	<h1 className="text-2xl font-bold text-slate-900">E-Voting OSIS</h1>
-    	</main>
-  	);
-	}
+  redirect("/vote");
+}
